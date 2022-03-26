35b9c062f5e8115022b2d94be17ea84d677aa370
Yaqoob <yaqoob@gmail.com>
2022-03-26 12:38:51 +0500

    Nav Component

--- app/src/main/java/com/orbitalsonic/navigationcomponentexample/HomeFragment.kt
+++ app/src/main/java/com/orbitalsonic/navigationcomponentexample/HomeFragment.kt
@@ -4,14 +4,13 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import androidx.fragment.app.Fragment
+import androidx.navigation.NavOptions
+import androidx.navigation.fragment.findNavController
 
 
 class HomeFragment : Fragment() {
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -20,4 +19,47 @@ class HomeFragment : Fragment() {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_home, container, false)
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val navController = findNavController()
+
+        view.findViewById<Button>(R.id.btn_send_money).setOnClickListener {
+            // Navigate By Safe Arg
+            val action = HomeFragmentDirections.actionHomeFragmentToChooseReceiverFragment()
+            navController.navigate(action)
+        }
+
+        view.findViewById<Button>(R.id.btn_view_balance).setOnClickListener {
+            // Navigate By Safe Arg
+            val action = HomeFragmentDirections.actionHomeFragmentToViewBalanceFragment()
+            navController.navigate(action)
+
+            // Navigate By ID
+//            navController.navigate(R.id.viewBalanceFragment)
+
+            // Navigate By ID and animation
+//            val navOption = NavOptions.Builder()
+//                .setEnterAnim(R.anim.slide_in_right)
+//                .setEnterAnim(R.anim.slide_out_left)
+//                .setPopEnterAnim(R.anim.slide_in_left)
+//                .setPopExitAnim(R.anim.slide_out_right).build()
+//
+//            navController.navigate(R.id.viewBalanceFragment,null, navOption)
+        }
+
+        view.findViewById<Button>(R.id.btn_transaction).setOnClickListener {
+            // Navigate By Safe Arg
+            val action = HomeFragmentDirections.actionHomeFragmentToViewTransactionFragment()
+            navController.navigate(action)
+
+            // Navigate By Action
+//            navController.navigate(R.id.action_homeFragment_to_viewTransactionFragment)
+
+
+        }
+
+    }
+
 }
