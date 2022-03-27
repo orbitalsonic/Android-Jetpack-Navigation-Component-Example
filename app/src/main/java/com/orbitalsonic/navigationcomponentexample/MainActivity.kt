6ab50730d3b71f43e89dde5711ea95016f7499c4
Yaqoob <yaqoob@gmail.com>
2022-03-27 16:45:33 +0500

    Nav Component

--- app/src/main/java/com/orbitalsonic/navigationcomponentexample/MainActivity.kt
+++ app/src/main/java/com/orbitalsonic/navigationcomponentexample/MainActivity.kt
@@ -2,10 +2,32 @@ package com.orbitalsonic.navigationcomponentexample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.widget.Toolbar
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.setupActionBarWithNavController
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var navController:NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val toolbar =findViewById<Toolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
+
+        val navHostFragment = supportFragmentManager
+            .findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        navController = navHostFragment.findNavController()
+
+        setupActionBarWithNavController(navController)
+//        toolbar.setupWithNavController(navController)
+
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
     }
 }
